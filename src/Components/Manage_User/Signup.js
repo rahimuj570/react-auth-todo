5baa2e5d1d8dec313ee855fadc2bd2e1eda70756
rahimuj570 <rahimuj570@gmail.com>
2022-04-15 22:36:59 +0600

    signup Ui added

--- src/Components/Manage_User/Signup.js
+++ src/Components/Manage_User/Signup.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import SocialSignin from "./SocialSignin";
+import { FaRegEye } from "react-icons/fa";
 
 const Signup = () => {
-  return <div></div>;
+  const [showPass, setShowPass] = useState(false);
+  const navigate = useNavigate();
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="flex flex-col justify-center"
+      >
+        <input
+          className="border my-1 p-1 w-5/6 sm:w-3/6 md:w-2/6 mx-auto"
+          required
+          placeholder="Your Name"
+          type="text"
+          name=""
+          id=""
+        />
+        <input
+          className="border my-1 p-1 w-5/6 sm:w-3/6 md:w-2/6 mx-auto"
+          required
+          placeholder="Your Email"
+          type="email"
+          name=""
+          id=""
+        />
+        <div className="relative border my-1  w-5/6 sm:w-3/6 md:w-2/6 mx-auto ">
+          <input
+            className="p-1 w-full"
+            required
+            placeholder="Your Password"
+            type={showPass ? "text" : "password"}
+          />
+          <FaRegEye
+            onClick={() => setShowPass(!showPass)}
+            title="Show Password"
+            className="cursor-pointer absolute inset-y-2.5 right-2"
+          />
+        </div>
+
+        <input
+          className="border my-1 p-1 w-5/6 sm:w-3/6 md:w-2/6 mx-auto"
+          required
+          placeholder="Confirm Password"
+          type={showPass ? "text" : "password"}
+        />
+
+        <input
+          className="mt-1 bg-sky-400 text-white w-5/6 sm:w-3/6 md:w-2/6 mx-auto p-1 rounded font-bold hover:bg-sky-500 duration-200"
+          type="submit"
+          value="Log In"
+        />
+        <p className="mt-2 text-center">
+          Already have an account?{" "}
+          <span
+            onClick={() => navigate("/login")}
+            className="cursor-pointer font-semibold hover:text-sky-500   duration-200 text-sky-400"
+          >
+            Log In
+          </span>
+        </p>
+      </form>
+      <SocialSignin />
+    </>
+  );
 };
 
 export default Signup;
