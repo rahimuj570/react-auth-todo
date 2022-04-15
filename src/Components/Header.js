f35682d11d44b9b1a9fb392ca29c564b3e698288
rahimuj570 <rahimuj570@gmail.com>
2022-04-15 23:01:35 +0600

    footer svg added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 import { FcTodoList } from "react-icons/fc";
 const Header = () => {
   return (
-    <div className="my-6 flex justify-center items-center">
-      <FcTodoList className="sm:text-6xl text-5xl" />
-      <p className="text-center sm:text-5xl text-4xl font-bold text-sky-400">
-        ToDo APP
-      </p>
-    </div>
+    <>
+      <div className="my-6 flex justify-center items-center">
+        <FcTodoList className="sm:text-6xl text-5xl" />
+        <p className="text-center sm:text-5xl text-4xl font-bold text-sky-400">
+          ToDo APP
+        </p>
+      </div>
+    </>
   );
 };
 
