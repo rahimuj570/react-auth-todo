84e3c068363ae8f2abd6b63188c2c7ea999ba985
rahimuj570 <rahimuj570@gmail.com>
2022-04-15 22:43:24 +0600

    footer added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  const year = new Date().getFullYear();
+  return (
+    <>
+      <p className="mt-10 mb-2 bg-slate-50 p-2 text-center">
+        ToDo APP &copy; {year} by{" "}
+        <span className="font-semibold italic">Rahimuj570</span>
+      </p>
+    </>
+  );
 };
 
 export default Footer;
