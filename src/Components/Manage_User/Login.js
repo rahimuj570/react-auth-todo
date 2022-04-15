4d2806950857820ecc6842356a5ba68f1b38ccb2
rahimuj570 <rahimuj570@gmail.com>
2022-04-15 22:29:22 +0600

    social signin UI added

--- src/Components/Manage_User/Login.js
+++ src/Components/Manage_User/Login.js
@@ -1,54 +1,58 @@
 import React, { useState } from "react";
 import { FaRegEye } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import SocialSignin from "./SocialSignin";
 
 const Login = () => {
   const [showPass, setShowPass] = useState(false);
   const navigate = useNavigate();
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-      className="flex flex-col justify-center"
-    >
-      <input
-        className="border my-1 p-1 w-5/6 sm:w-4/6 mx-auto"
-        required
-        placeholder="Your Email"
-        type="email"
-        name=""
-        id=""
-      />
-      <div className="relative border my-1  w-5/6 sm:w-4/6 mx-auto ">
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="flex flex-col justify-center"
+      >
         <input
-          className="p-1 w-full"
+          className="border my-1 p-1 w-5/6 sm:w-3/6 md:w-2/6 mx-auto"
           required
-          placeholder="Your Password"
-          type={showPass ? "text" : "password"}
+          placeholder="Your Email"
+          type="email"
+          name=""
+          id=""
         />
-        <FaRegEye
-          onClick={() => setShowPass(!showPass)}
-          title="Show Password"
-          className="cursor-pointer absolute inset-y-2.5 right-2"
-        />
-      </div>
+        <div className="relative border my-1  w-5/6 sm:w-3/6 md:w-2/6 mx-auto ">
+          <input
+            className="p-1 w-full"
+            required
+            placeholder="Your Password"
+            type={showPass ? "text" : "password"}
+          />
+          <FaRegEye
+            onClick={() => setShowPass(!showPass)}
+            title="Show Password"
+            className="cursor-pointer absolute inset-y-2.5 right-2"
+          />
+        </div>
 
-      <input
-        className="mt-1 bg-sky-400 text-white w-5/6 sm:w-4/6 mx-auto p-1 rounded font-bold hover:bg-sky-500 duration-200"
-        type="submit"
-        value="Log In"
-      />
-      <p className="mt-2 text-center">
-        Don't have an account?{" "}
-        <span
-          onClick={() => navigate("/signup")}
-          className="cursor-pointer font-semibold hover:text-sky-500   duration-200 text-sky-400"
-        >
-          Create An Account
-        </span>
-      </p>
-    </form>
+        <input
+          className="mt-1 bg-sky-400 text-white w-5/6 sm:w-3/6 md:w-2/6 mx-auto p-1 rounded font-bold hover:bg-sky-500 duration-200"
+          type="submit"
+          value="Log In"
+        />
+        <p className="mt-2 text-center">
+          Don't have an account?{" "}
+          <span
+            onClick={() => navigate("/signup")}
+            className="cursor-pointer font-semibold hover:text-sky-500   duration-200 text-sky-400"
+          >
+            Create An Account
+          </span>
+        </p>
+      </form>
+      <SocialSignin />
+    </>
   );
 };
 
--- src/Components/Manage_User/SocialSignin.js
+++ src/Components/Manage_User/SocialSignin.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub, FaFacebook } from "react-icons/fa";
 
 const SocialSignin = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="my-8 mx-2 items-center flex justify-center">
+        <div className="rounded h-1 sm:w-2/6 md:w-1/6 w-full bg-sky-200"></div>
+        <p className="font-semibold mx-2">OR</p>
+        <div className="rounded h-1 sm:w-2/6 md:w-1/6 w-full bg-sky-200"></div>
+      </div>
+
+      <div className="sm:w-2/6 w-5/6 mx-auto">
+        <div className="mb-3 duration-300  hover:bg-slate-50 rounded-md   py-2 text-xl flex items-center justify-center px-5 shadow">
+          <FcGoogle className="basis-10 text-3xl mx-1" />{" "}
+          <p className="">Continue With Google</p>
+        </div>
+        <div className="mb-3 duration-300 hover:bg-slate-50 rounded-md   py-2 text-xl flex items-center justify-center px-5 shadow">
+          <FaGithub className="basis-10  text-3xl mx-1" />{" "}
+          <p className="">Continue With Github</p>
+        </div>
+        <div className="mb-3 duration-300 hover:bg-slate-50 rounded-md   py-2 text-xl flex items-center justify-center px-5 shadow">
+          <FaFacebook className="basis-10 text-sky-600 text-3xl mx-1" />{" "}
+          <p className="">Continue With Facebook</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SocialSignin;
