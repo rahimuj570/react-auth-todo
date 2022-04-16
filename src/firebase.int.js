ff10b9e9cb6e3e6ab7ba1a36be3a0a1d3372816c
rahimuj570 <rahimuj570@gmail.com>
2022-04-16 13:40:26 +0600

    react env var added

--- src/firebase.int.js
+++ src/firebase.int.js
@@ -6,12 +6,12 @@ import { getAuth } from "react-firebase-hooks/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBY3iw88itITg0wEpcP83r-wl6T2Ji0Q5o",
-  authDomain: "react-auth-todo-f3bd0.firebaseapp.com",
-  projectId: "react-auth-todo-f3bd0",
-  storageBucket: "react-auth-todo-f3bd0.appspot.com",
-  messagingSenderId: "1032923645538",
-  appId: "1:1032923645538:web:060e78fd83b95cd049f33b",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
